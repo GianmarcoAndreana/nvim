abb0b51d9f03d4204358624ed92468a4a1cb31de
Gianmarco Andreana <42800762+GianmarcoAndreana@users.noreply.github.com>
2025-03-30 15:30:16 +0200

    configured neo-tree keybind

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -11,7 +11,7 @@ return {
   },
   cmd = 'Neotree',
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<C-e>', '<cmd>Neotree toggle left<CR>', desc = 'Toggle Neo-tree' },
   },
   opts = {
     filesystem = {
