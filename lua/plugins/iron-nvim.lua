f12335bb588ad3080c6382b701372698b779d413
Gianmarco Andreana <42800762+GianmarcoAndreana@users.noreply.github.com>
2025-04-09 14:24:17 +0200

    optimized config (LSP and iron.nvim)

--- lua/plugins/iron-nvim.lua
+++ lua/plugins/iron-nvim.lua
@@ -1,29 +1,24 @@
 return {
   'Vigemus/iron.nvim',
+  ft = { 'python' }, -- Only load when editing Python files
   config = function()
     local iron = require 'iron.core'
     local view = require 'iron.view'
 
     iron.setup {
       config = {
-        -- Define how the REPL window will be displayed
-        repl_open_cmd = view.split.vertical.botright(80),
-        -- Define REPL configurations for specific file types
+        repl_open_cmd = view.split.vertical.botright(80), -- Open REPL in a vertical split
         repl_definition = {
           python = {
-            -- Can be a table or a function that returns a table
             command = { 'ipython', '--no-autoindent' },
-          },
-          -- Add other languages here
+            format = require('iron.fts.common').bracketed_paste,
+          }, -- Add other language REPLs here if needed
         },
-        -- Other configurations can be added here
       },
-      -- Iron doesn't set keymaps by default anymore.
-      -- You can set them here or manually add keymaps later.
       keymaps = {
         send_motion = '<space>sc',
         visual_send = '<space>sc',
-        send_file = '<space>sf',
+        send_file = '<space>sa',
         send_line = '<space>sl',
         send_paragraph = '<space>sp',
         send_until_cursor = '<space>su',
@@ -32,17 +27,17 @@ return {
         mark_visual = '<space>mc',
         remove_mark = '<space>md',
         cr = '<space>s<cr>',
-        interrupt = '<space>s<space>',
+        interrupt = '<space>rq<space>',
         exit = '<space>sq',
         clear = '<space>cl',
       },
-      -- Highlighting settings
-      highlight = {
-        italic = true,
-      },
-      ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
+      highlight = { italic = true },
+      ignore_blank_lines = true,
     }
+
     -- Keymap to start the REPL
     vim.keymap.set('n', '<space>rs', '<cmd>IronRepl<cr>', { desc = 'Start REPL' })
+    -- Keymap to quit the REPL
+    vim.keymap.set('n', '<space>rr', '<cmd>IronRestart<cr>', { desc = 'Restart REPL' })
   end,
 }
