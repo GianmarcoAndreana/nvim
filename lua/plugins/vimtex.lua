6f262b566b0487f0e7b28c669e8829f3534eccd1
Gianmarco Andreana <42800762+GianmarcoAndreana@users.noreply.github.com>
2025-04-23 16:33:16 +0400

    introducing autocleaning of auxiliary files in tex (fixed)

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -9,16 +9,39 @@ return {
     -- Automatically close quickfix after using `\ll`
     vim.g.vimtex_quickfix_autoclose_after_keystrokes = 1
 
-    -- Optional: Define a command for manual cleaning
+    -- Automatically remove auxiliary files
     vim.api.nvim_create_user_command('LatexClean', function()
-      vim.fn.jobstart({ 'latexmk', '-c' }, { cwd = vim.fn.expand '%:p:h' })
-    end, { desc = 'Clean LaTeX build files with latexmk -c' })
+      local tex_dir = vim.fn.expand '%:p:h' -- Directory of the .tex file
 
-    -- Auto clean auxiliary files on buffer leave (closing .tex files)
+      -- List of common auxiliary files to remove
+      local extensions = { '.aux', '.fls', '.log', '.nav', '.out', '.toc', '.pdfsync', 'snm', '.fdb_latexmk', '.synctex' }
+
+      for _, ext in ipairs(extensions) do
+        -- Find all files with the current extension in the directory
+        local files = vim.fn.glob(tex_dir .. '\\*' .. ext, false, true)
+
+        -- Loop through the files and delete them
+        for _, file in ipairs(files) do
+          if vim.fn.filereadable(file) == 1 then
+            vim.fn.delete(file)
+            -- Suppress the message by removing the print statement
+          end
+        end
+      end
+    end, { desc = 'Manually clean LaTeX auxiliary files' })
+
+    -- Autocmd to clean auxiliary files when closing a .tex file
     vim.api.nvim_create_autocmd('BufWinLeave', {
       pattern = '*.tex',
       callback = function()
-        vim.fn.jobstart({ 'latexmk', '-c' }, { cwd = vim.fn.expand '%:p:h' })
+        vim.cmd 'LatexClean' -- Call the LatexClean command
+      end,
+    })
+
+    -- Autocmd to clean auxiliary files when Neovim is exited
+    vim.api.nvim_create_autocmd('VimLeave', {
+      callback = function()
+        vim.cmd 'LatexClean' -- Call the LatexClean command
       end,
     })
   end,
