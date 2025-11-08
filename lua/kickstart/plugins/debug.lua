6e783006910d9fc7a99153a651c541642024a5e7
Gianmarco Andreana <42800762+GianmarcoAndreana@users.noreply.github.com>
2025-11-08 18:56:01 +0100

    Fixed icons and improve the debugger for python

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -22,7 +22,6 @@ return {
     'jay-babu/mason-nvim-dap.nvim',
 
     -- Add your own debuggers here
-    'leoluz/nvim-dap-go',
     'mfussenegger/nvim-dap-python',
   },
   keys = {
@@ -95,7 +94,6 @@ return {
       -- online, please don't ask me how to install them :)
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
-        'delve',
         'python',
       },
     }
@@ -138,14 +136,19 @@ return {
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
-    -- Install golang specific config
-    require('dap-go').setup {
-      delve = {
-        -- On Windows delve must be run attached or it crashes.
-        -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
-        detached = vim.fn.has 'win32' == 0,
-      },
-    }
-    require('dap-python').setup()
+    -- ==== Python: choose a working interpreter ====
+    local function pick_python()
+      local exepath = vim.fn.exepath
+      if exepath 'python3' ~= '' then
+        return exepath 'python3'
+      elseif exepath 'python' ~= '' then
+        return exepath 'python'
+      else
+        return 'python3' -- last resort; may still fail if truly missing
+      end
+    end
+
+    -- Install specific config
+    require('dap-python').setup(pick_python())
   end,
 }
