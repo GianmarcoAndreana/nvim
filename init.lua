939620de202033c0f1d0448ca7e5dbf4b019d0ff
Gianmarco Andreana <42800762+GianmarcoAndreana@users.noreply.github.com>
2025-11-08 22:14:11 +0100

    Added powershell support

--- init.lua
+++ init.lua
@@ -75,6 +75,12 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- Use Powershell as the default terminal shell
+vim.opt.shell = 'pwsh'
+vim.opt.shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command'
+vim.opt.shellquote = '"'
+vim.opt.shellxquote = ''
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>
 
